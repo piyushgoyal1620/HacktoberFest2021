5e66bf36f109d804ce59c1d924b230944a6f5c0c
Deepanshu Singh <deepanshu2002d@gmail.com>
2020-10-21 22:35:05 +0530

    Can convert temperature unit via CLI

---
+++ Temperature-Converter-via-CLI.py
@@ -0,0 +1,18 @@
+import sys
+
+num=sys.argv[1]
+# num = input("Choose a number\n")
+unit=sys.argv[2]
+# choose Celsius or Fahrenheit
+# unit = input("Choose unit: Celsius or Fahrenheit\n")
+
+# convert Celsius to Fahrenheit
+if unit.lower() == "Celsius".lower():
+    temp = int(num) * 9/5 + 32
+    print(num, "degrees Celsius converts to", temp, "degrees Fahrenheit.")
+elif unit.lower() == "Fahrenheit".lower():
+    temp = (int(num) - 32) / (9/5)
+    print(num, "degrees Fahrenheit converts to", temp, "degrees Celsius.")
+else:
+    # I always mispell Fahrenheit lol
+    print("Please check your unit spelling!")
