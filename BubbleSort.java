295b5f91f54c5de215c821b3d97a34745f590eda
Ankit Kumar Singh <72161660+ankit-singh706@users.noreply.github.com>
2021-10-19 23:21:17 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,25 +1,50 @@
-package com.houarizegai.algorithms.sort.bubblesort;
 
-import com.houarizegai.algorithms.Utils;
+import java.io.*;
 
-public class BubbleSort {
-    public static void main(String[] args) {
-        int[] array = Utils.generateRandomTable(15, 50);
-        Utils.displayTable(array);
-        bubbleSort(array);
-        Utils.displayTable(array);
-    }
+class GFG
+{
+	// An optimized version of Bubble Sort
+	static void bubbleSort(int arr[], int n)
+	{
+		int i, j, temp;
+		boolean swapped;
+		for (i = 0; i < n - 1; i++)
+		{
+			swapped = false;
+			for (j = 0; j < n - i - 1; j++)
+			{
+				if (arr[j] > arr[j + 1])
+				{
+					// swap arr[j] and arr[j+1]
+					temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+					swapped = true;
+				}
+			}
 
-    public static void bubbleSort(int[] array) {
-        int size = array.length;
-        for(int i = 0; i < size; i++) {
-            for(int j = 0; j < size - i - 1; j++) {
-                if(array[j] > array[j + 1]) {
-                    array[j] = array[j] + array[j + 1];
-                    array[j + 1] = array[j] - array[j + 1];
-                    array[j] = array[j] - array[j + 1];
-                }
-            }
-        }
-    }
+			
+			if (swapped == false)
+				break;
+		}
+	}
+
+	// Function to print an array
+	static void printArray(int arr[], int size)
+	{
+		int i;
+		for (i = 0; i < size; i++)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+
+	
+	public static void main(String args[])
+	{
+		int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+		int n = arr.length;
+		bubbleSort(arr, n);
+		System.out.println("Sorted array: ");
+		printArray(arr, n);
+	}
 }
