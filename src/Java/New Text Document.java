664ef2c83c9b7e2d300fe6796de28e557f656457
Mahi Monga <mahimonga@users.noreply.github.com>
2021-10-25 02:47:37 +0530

    moved files to their respective folders and improved repo design

---
+++ src/Java/New Text Document.java
@@ -0,0 +1,10 @@
+package com.company;
+import java.util.Scanner;
+public class hacktober {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter your name:");
+    String name = sc.next();
+    System.out.println("Welcome to Hacktober" + name +  "Fest 2021!");
+  }
+}
